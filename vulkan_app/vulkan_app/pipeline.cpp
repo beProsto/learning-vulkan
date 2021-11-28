f945e6632d95db0529598f242816781e7b0b0362
beProsto <lavionperavion2@gmail.com>
2021-11-29 00:10:19 +0100

    shader stage creation infos

--- vulkan_app/vulkan_app/pipeline.cpp
+++ vulkan_app/vulkan_app/pipeline.cpp
@@ -8,22 +8,45 @@
 #include <iostream>
 
 VkShaderModule createShaderModule(const std::string& _code, VkDevice _device);
+VkPipelineShaderStageCreateInfo getShaderStageCreateInfo(VkShaderModule _module, VkShaderStageFlagBits _type);
 
 // We create our graphics pipeline
 void vulkan_app::create_pipeline()
 {
+	// We read the shaders from files
 	std::string vertexShaderCode = readFile("./triangle_v.spv");
 	std::string fragmentShaderCode = readFile("./triangle_f.spv");
 
 	std::cout << "\nVERT SHADER CODE SIZE: " << vertexShaderCode.size() << "\nFRAG SHADER CODE SIZE: " << fragmentShaderCode.size() << "\n";
 
+	// We create shader modules out of the shaders' code
 	VkShaderModule vertexShaderMod = createShaderModule(vertexShaderCode, device);
 	VkShaderModule fragmentShaderMod = createShaderModule(fragmentShaderCode, device);
 
-	// code lol
+	// We define stage creation infos for both the vertex shader stage and the fragment shader stage
+	VkPipelineShaderStageCreateInfo shaderStagesCreationInfos[2] = {
+		getShaderStageCreateInfo(vertexShaderMod, VK_SHADER_STAGE_VERTEX_BIT),
+		getShaderStageCreateInfo(fragmentShaderMod, VK_SHADER_STAGE_FRAGMENT_BIT)
+	};
+	// Now shaderStagesCreationInfos[0] is for the vertex shader stage and shaderStagesCreationInfos[1] if for the fragment shader stage
 
+	
+
+	// We free the shader modules
 	vkDestroyShaderModule(device, fragmentShaderMod, nullptr);
 	vkDestroyShaderModule(device, vertexShaderMod, nullptr);
+}
+
+// Gets the shader stages' create infos
+VkPipelineShaderStageCreateInfo getShaderStageCreateInfo(VkShaderModule _module, VkShaderStageFlagBits _type)
+{
+	VkPipelineShaderStageCreateInfo shaderStageInfo{};
+	shaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	shaderStageInfo.stage = _type;
+	shaderStageInfo.module = _module;
+	shaderStageInfo.pName = "main";
+
+	return shaderStageInfo;
 }
 
 // Shader Module Creation (sub shaders / shaders in opengl)
